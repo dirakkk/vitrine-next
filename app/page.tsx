8e41dc46c29260cfbb7db17012213157f1d506b1
yannser <yannick.serra@darva.com>
2024-12-14 16:32:57 +0100

    few mod

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by jolling <code>app/page.tsx</code>.
+            Get started by running <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
