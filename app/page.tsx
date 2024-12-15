6417c3e6d3c934999d0e0de0bf7b275ac9a15d3b
yannser <yannick.serra@darva.com>
2024-12-15 18:24:24 +0100

    few mod

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            RENOLD to Read our docs
+            RENOLD SER to Read our docs
           </a>
         </div>
       </main>
