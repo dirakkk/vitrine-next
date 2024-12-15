4b2231b0769805795e2cb1e40fa56b4f8246393a
yannser <yannick.serra@darva.com>
2024-12-15 10:38:27 +0100

    few mod

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            NO to Read our docs
+            NO to Reados our docs
           </a>
         </div>
       </main>
