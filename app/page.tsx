e3c0b51bff215109d41cd3e0bdbb9edcb9df4466
yannser <yannick.serra@darva.com>
2024-12-16 12:21:43 +0100

    wazza

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Adrien
+            Adrien est en place
           </a>
         </div>
       </main>
